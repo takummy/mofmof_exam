2a618633d626afacbfe51060c42148773ced147a
Your Actual Name <Your Actual Email>
2018-10-24 11:26:10 +0900

    from及びvalidationを修正

--- app/models/building.rb
+++ app/models/building.rb
@@ -3,8 +3,8 @@ class Building < ApplicationRecord
   accepts_nested_attributes_for :nearest_stations, allow_destroy: true, reject_if: :all_blank
 
   validates :name,    presence: true
-  validates :rent,    presence: true,         numericality: {only_integer: true, greater_than: 1}
+  validates :rent,    presence: true,         numericality: {only_integer: true, greater_than: 0}
   validates :address, presence: true,         length: {maximum: 50}
-  validates :age,     presence: true,         numericality: {only_integer: true, greater_than: 1}
-  validates :notes,   length: {maximum: 255}, allow_blank: true
+  validates :age,     presence: true,         numericality: {only_integer: true, greater_than: 0}
+  validates :notes,   length: {maximum: 400}, allow_blank: true
 end
