db79eede342b02ecbde3a43bb503c126becaa05d
Your Actual Name <Your Actual Email>
2018-10-24 16:41:32 +0900

    reject_ifを追加

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,6 +1,6 @@
 class Building < ApplicationRecord
-  has_many :nearest_stations, inverse_of: :building, dependent: :destroy
-  accepts_nested_attributes_for :nearest_stations, allow_destroy: true
+  has_many                       :nearest_stations, inverse_of: :building, dependent: :destroy
+  accepts_nested_attributes_for  :nearest_stations, allow_destroy: true, reject_if: :all_blank
 
   validates :name,    presence: true
   validates :rent,    presence: true,         numericality: {only_integer: true, greater_than: 0}
