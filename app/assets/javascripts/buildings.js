3a9c58ece150bdf338cdf9a1bbe4ce34a350c729
Your Actual Name <Your Actual Email>
2018-10-25 23:34:11 +0900

    jsを調整

--- app/assets/javascripts/buildings.js
+++ app/assets/javascripts/buildings.js
@@ -1,3 +1,11 @@
-$( document ).ready(function() {
+$(document).ready(function() {
   $(".field").trigger('refresh_autonumeric');
+
+  let count = 1
+  $('#js-nested_fields_link').on('click', function() {
+    setTimeout(function(){ 
+      $(this).data('click', ++count)
+      $('.nearest_stations:last').text(`最寄り駅${count}`)
+    }, 0);
+  });
 });
